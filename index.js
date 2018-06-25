7dd9e238e780c59e251af0efecfa3a05df933de9
Sehajbir Randhawa <srandhaw@purdue.edu>
2018-06-25 18:07:18 -0400

    HW-1 done

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("heye");
+const button = document.querySelector('button')
+var count = 0
+
+const update = function(){
+    const header = document.querySelector('h1')
+    if(count%2==0)
+    header.textContent = "changed"
+    else
+    header.textContent = "Chrismess"
+    count++
+}
+
+button.addEventListener('click',update)
